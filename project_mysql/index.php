78862e4c8794b719f58f15c289e83bab14dcd692
AdrielBernhard <adrielsmi79@gmail.com>
2025-04-10 17:31:04 +0700

    Delete project_mysql directory

--- project_mysql/index.php
+++
@@ -1,5 +0,0 @@
-
-<?php include 'template/header.php'; ?>
-<h1>Selamat datang di</h1>
-<h2 style="font-size: 60px; font-weight: bold;">PROJECT</h2>
-<?php include 'template/footer.php'; ?>
--- project_mysql/template/footer.php
+++
@@ -1,7 +0,0 @@
-
-</div> <!-- content-wrapper -->
-<script src="adminlte/plugins/jquery/jquery.min.js"></script>
-<script src="adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
-<script src="adminlte/js/adminlte.min.js"></script>
-</body>
-</html>
